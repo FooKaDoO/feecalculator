1ed00635dd3fa4a8e7cb3dc16d6a343251502cf7
FooKaDoO <mihkelhani@gmail.com>
2024-03-17 19:26:49 +0200

    Station constructor without name argument using StationEnum

--- app/src/main/java/com/feecalculator/app/entity/Station.java
+++ app/src/main/java/com/feecalculator/app/entity/Station.java
@@ -1,6 +1,7 @@
 package com.feecalculator.app.entity;
 
 
+import com.feecalculator.app.enums.StationEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,4 +34,26 @@ public class Station {
 
     @Setter
     private Long timestamp;
+
+    /**
+     * Constructor for creating a Station entity, without using station name.<br>
+     * Uses StationEnum to map WMO code to station name.
+     * @param wmocode WMO code of station.
+     * @param airtemperature Air temperature at the given time (timestamp) from the station.
+     * @param windspeed Wind speed at the given time (timestamp) from the station.
+     * @param phenomenon Weather phenomenon at the given time (timestamp) from the station.
+     * @param timestamp Timestamp of the measurement.
+     */
+    public Station(Integer wmocode,
+                   Double airtemperature,
+                   Double windspeed,
+                   String phenomenon,
+                   Long timestamp) {
+        this.name = StationEnum.fromWMOCode(wmocode).label;
+        this.wmocode = wmocode;
+        this.airtemperature = airtemperature;
+        this.windspeed = windspeed;
+        this.phenomenon = phenomenon;
+        this.timestamp = timestamp;
+    }
 }
