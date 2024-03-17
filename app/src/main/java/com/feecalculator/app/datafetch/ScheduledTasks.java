f97285a9b4952e93698b3f5388e18062fc9e3ec3
FooKaDoO <mihkelhani@gmail.com>
2024-03-17 20:46:25 +0200

    Methods for scheduled fetching

--- app/src/main/java/com/feecalculator/app/datafetch/ScheduledTasks.java
+++ app/src/main/java/com/feecalculator/app/datafetch/ScheduledTasks.java
@@ -1,23 +1,49 @@
 package com.feecalculator.app.datafetch;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.feecalculator.app.entity.Station;
+import com.feecalculator.app.entity.XML.Observations;
 import com.feecalculator.app.service.DatafetchService;
+import com.feecalculator.app.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
 
     @Autowired
     DatafetchService datafetchService;
+    @Autowired
+    StationService stationService;
 
+    /**
+     * URI taken from application.properties.
+     */
     @Value("${custom.URI}")
     private String URI;
 
+    /**
+     * Scheduling taken from application.properties.<br>
+     * Fetches data by URI.<br>
+     * Maps all Observations entity stations to a list of stations. <br>
+     * Saves the list of stations to the database. <br>
+     * <br>
+     * In case of XML parsing errors, prints them to the terminal.
+     */
     @Scheduled(cron = "${custom.cron}")
     public void getData() {
         String data = datafetchService.fetchDataFromURI(URI);
-        System.out.println("Cron");
+        try {
+            Observations observations = datafetchService.mapXMLtoEntity(data);
+            List<Station> stations = stationService.mapObservationToStationList(observations);
+            stationService.saveAllStationsToDB(stations);
+        } catch (JsonProcessingException e) {
+            System.out.println("XML parsing exception:");
+            System.out.println(e.getMessage());
+        }
     }
 }
